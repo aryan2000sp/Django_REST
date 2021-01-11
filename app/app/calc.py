b2647e005cddc3682d9192ea8c823732959cf4bb
Aryan <aryan2000sp@gmail.com>
2021-01-10 19:05:46 -0500

    Added

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+    This is a simple function that will add 
+    two numbers. This is function is created 
+    for the purpose to learn unit testing.
+"""
+def add(num1, num2):
+    return num1+num2
---
+++ app/app/test.py
@@ -0,0 +1,31 @@
+"""
+    This file will handle all testing
+    done through out the project.
+    
+    NOTE:-
+        The name of the .py file should 
+        be test because when we run the 
+        command 'py manage.py test' it looks
+        for the file with name test.py and runs it.
+        Also the name name of the function should 
+        start with test (eg:- def test_someFinction(): )
+"""
+
+"""
+    Django has buildin class called 
+    TestCase that will have some methods 
+    associated with the unit testing.
+"""
+from django.test import TestCase
+from app.calc import add
+
+"""
+    Now we are write a class that extends the 
+    TestCase so that we can use the some 
+    functions from the TestCase. 
+"""
+class CalcTests(TestCase):
+
+    def test_calcAdd(self):
+        #Test the calc add() function
+        self.assertEqual(add(1,10), 11)
